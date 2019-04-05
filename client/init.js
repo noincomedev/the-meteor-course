087ff77d3d3a5d16b5a5fe9e5c1bc2deed4ca34a
Diego Robles <diego@hemployeed.com>
2019-04-05 12:34:10 -0300

    chore(base):update client init

--- client/init.js
+++ client/init.js
@@ -1,3 +1,2 @@
 console.log("CLIENT INITIALIZED");
-console.log("AS METEOR RUN LOOKS FOR ./server FOLDER");
-console.log("TO INIT SERVER, IT LOOKS FOR ./client to init client side!");
+console.log("lets add our first HTML file!");

31d8e3e94c242c947053d5f9342aa8fe30bd3935
Diego Robles <diego@hemployeed.com>
2019-04-05 11:32:32 -0300

    chore(base):init server

---
+++ server/init.js
@@ -0,0 +1,6 @@
+console.log("SERVER INITIALIZED");
+console.log("==================");
+console.log(`NAME: ${Meteor.settings.public.name}`);
+console.log("If you do not see NAME APP");
+console.log("RESTART WITH NPM RUN DEV");
+console.log("==================");

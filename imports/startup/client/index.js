49b97571a95b6e5c838053dc46b537138996bee1
Diego Robles <diego@hemployeed.com>
2019-04-08 14:40:15 -0400

    feat(base):init react

--- imports/startup/client/index.js
+++ imports/startup/client/index.js
@@ -1,3 +1,7 @@
 import { Meteor } from "meteor/meteor";
+import { render } from "react-dom";
+import React from "react";
 
-Meteor.startup(() => {});
+Meteor.startup(() => {
+  render(<h1>Hello React</h1>, document.getElementById("react-root"));
+});

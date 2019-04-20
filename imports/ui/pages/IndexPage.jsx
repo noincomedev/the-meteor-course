b26b3896f326e5300a03a477a06f1e0af21515d2
Diego Robles <diego@hemployeed.com>
2019-04-20 00:25:02 -0400

    feat(todos):add dummy list

--- imports/ui/pages/IndexPage.jsx
+++ imports/ui/pages/IndexPage.jsx
@@ -1,3 +1,16 @@
 import React from "react";
 
-export default () => <h1>INDEX</h1>;
+import Todos from "../components/lists/todos/List";
+
+const TODOS = [{ _id: "asd123", title: "TODO1", done: false }];
+
+export default () => (
+  <div className="container col">
+    <nav className="nav">
+      <h1 className="headline">
+        {Meteor.settings.public.constants.pages.index.HEADLINE}
+      </h1>
+    </nav>
+    <Todos todos={TODOS} />
+  </div>
+);

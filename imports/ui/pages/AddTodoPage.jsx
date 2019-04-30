647f829ed53a4b28894e5a985dabf4bf5658404d
Diego Robles <diego@hemployeed.com>
2019-04-29 23:33:48 -0400

    fix(accounts):fixed navbar and styles

--- imports/ui/pages/AddTodoPage.jsx
+++ imports/ui/pages/AddTodoPage.jsx
@@ -3,7 +3,7 @@ import React from "react";
 import TodoForm from "../components/forms/TodoForm";
 
 export default () => (
-  <div className="container justify-content-center">
+  <div className="container align-items-center justify-content-center">
     <TodoForm />
   </div>
 );
--- imports/ui/pages/IndexPage.jsx
+++ imports/ui/pages/IndexPage.jsx
@@ -1,3 +1,9 @@
 import React from "react";
 
-export default () => <h1>INDEX</h1>;
+export default () => (
+  <div className="container align-items-center justify-content-center">
+    <div className="col col-1-3">
+      <h1>INDEX</h1>
+    </div>
+  </div>
+);
